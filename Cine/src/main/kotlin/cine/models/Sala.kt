f677beb2e7f408502e10018140b17e85b98b2bdf
Samuel Cortés Sánchez <132077654+Samuceese@users.noreply.github.com>
2024-04-22 19:45:47 +0200

    Creacion rama cine y models con sala

---
+++ Cine/src/main/kotlin/cine/models/Sala.kt
@@ -0,0 +1,10 @@
+package cine.models
+
+import butacas.models.Butaca
+
+class Sala(
+    val filas: Int = 5,
+    val columnas: Int = 7,
+    val butacas : List<List<Butaca>> = List(filas) { List(columnas) { Butaca() } }
+) {
+}
