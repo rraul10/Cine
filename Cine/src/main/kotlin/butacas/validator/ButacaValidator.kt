cd197370c278a1cbfee4916f190e149c5cfe4f33
Samuel Cortés Sánchez <132077654+Samuceese@users.noreply.github.com>
2024-04-22 16:18:07 +0200

    Probando el validator

--- Cine/src/main/kotlin/butacas/validator/ButacaValidator.kt
+++ Cine/src/main/kotlin/butacas/validator/ButacaValidator.kt
@@ -1,6 +1,25 @@
 package butacas.validator
 
+import butacas.errors.ButacaErrors
+import butacas.models.Butaca
+import com.github.michaelbull.result.Err
+import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.Result
+import org.lighthousegames.logging.logging
+
+private val logger = logging()
+
 class ButacaValidator {
+
+    fun validate(butaca: Butaca): Result<Butaca, ButacaErrors> {
+        logger.debug{"Validando butaca"}
+        return if (!esValidaButaca(butaca.id)) {
+            Err(ButacaErrors.ButacaNotValid("La butaca no es valida"))
+        } else {
+            Ok(butaca)
+        }
+    }
+
 
     fun esValidaButaca(id: String): Boolean {
         val regex = Regex("[A-F][1-7]")
