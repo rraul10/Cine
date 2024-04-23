ab1e18fbcab50705b5ed5e9319df7e3270e8e637
Raul Fernandez <raulfernandez6106@gmail.com>
2024-04-23 12:11:00 +0200

    Creación ventasError

--- Cine/src/main/kotlin/ventas/error/VentasError.kt
+++ Cine/src/main/kotlin/ventas/error/VentasError.kt
@@ -1,4 +1,8 @@
 package ventas.error
 
-class VentasError {
+sealed class VentasError(val message: String) {
+    class VentasNoEncontradas(message: String) : VentasError(message)
+    class VentasNoValidas(message: String) : VentasError(message)
+    class VentasNoAlmacenadas(message: String) : VentasError(message)
+    class VentasStorageError(message: String) : VentasError(message)
 }
