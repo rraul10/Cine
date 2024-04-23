1e9b1577c75f7ad2e6c391fe3d40061290c8c8ed
Samuel Cortés Sánchez <132077654+Samuceese@users.noreply.github.com>
2024-04-23 23:09:34 +0200

    ButacaRepository

--- Cine/src/main/kotlin/butacas/services/ButacasService.kt
+++ Cine/src/main/kotlin/butacas/services/ButacasService.kt
@@ -1,16 +1,16 @@
 package butacas.services
 
 
-import butacas.errors.ButacaError
+import butacas.exceptions.ButacaException
 import butacas.models.Butaca
 import com.github.michaelbull.result.Result
 import java.io.File
 
 interface ButacasService {
-    fun getAll(): Result<List<Butaca>, ButacaError>
-    fun getById(id: String): Result<Butaca, ButacaError>
-    fun create(butaca: Butaca): Result<Butaca, ButacaError>
-    fun update(id: String, butaca: Butaca): Result<Butaca, ButacaError>
-    fun delete(id: String): Result<Butaca, ButacaError>
-    fun import(csvFile: File): Result<Unit, ButacaError>
+    fun getAll(): Result<List<Butaca>, ButacaException>
+    fun getById(id: String): Result<Butaca, ButacaException>
+    fun create(butaca: Butaca): Result<Butaca, ButacaException>
+    fun update(id: String, butaca: Butaca): Result<Butaca, ButacaException>
+    fun delete(id: String): Result<Butaca, ButacaException>
+    fun import(csvFile: File): Result<Unit, ButacaException>
 }
