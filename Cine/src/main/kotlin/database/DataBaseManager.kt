ce21a22d0700b9f15bd11c1711e53c4e0967dcea
Raul Fernandez <raulfernandez6106@gmail.com>
2024-04-25 19:11:43 +0200

    Eliminar paquetes socio.

---
+++ Cine/src/main/kotlin/database/DataBaseManager.kt
@@ -0,0 +1,11 @@
+package database
+
+import config.Config
+import org.lighthousegames.logging.logging
+import java.io.PrintWriter
+import java.io.Reader
+import java.sql.Connection
+import java.sql.DriverManager
+
+private val logger = logging()
+
