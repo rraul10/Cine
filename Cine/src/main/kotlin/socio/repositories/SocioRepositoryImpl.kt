66e2cd672373ba39f6a95e7e43a513538786e32e
Raul Fernandez <raulfernandez6106@gmail.com>
2024-04-26 16:02:36 +0200

    c

--- Cine/src/main/kotlin/socio/repositories/SocioRepositoryImpl.kt
+++ Cine/src/main/kotlin/socio/repositories/SocioRepositoryImpl.kt
@@ -1,46 +1,45 @@
 package socio.repositories
 
+import org.lighthousegames.logging.logging
 import socio.models.Socio
 import java.time.LocalDateTime
 
 private val logger = logging()
 
-vale este es el problema de esto, estoy haciendo el repository este de socio impl y hay q hacer el sqldelight manager para que no salga rojo sae
-en plan creabas el directory y luego haciamos un file
-class ClientesRepositoryImpl(
+class SocioRepositoryImpl(
     private val dbManager: SqlDeLightManager
 ) : ClientesRepository {
     private val db = dbManager.databaseQueries
 
     override fun findAll(): List<Socio> {
         logger.debug { "Buscando todos los socios" }
-        return db.selectAllClientes().executeAsList().map { it.toCliente() }
+        return db.selectAllSocio().executeAsList().map { it.toSocio() }
     }
 
     override fun findById(id: Long): Socio? {
-        logger.debug { "Buscando cliente por id: $id" }
-        return db.selectClienteById(id).executeAsOneOrNull()?.toCliente()
+        logger.debug { "Buscando socio por id: $id" }
+        return db.selectSocioById(id).executeAsOneOrNull()?.toSocio()
     }
 
-    override fun save(cliente: Socio): Socio {
+    override fun save(socio: Socio): Socio {
         logger.debug { "Guardando socio: $socio" }
 
         val timeStamp = LocalDateTime.now().toString()
 
         db.transaction {
-            db.insertCliente(
-                nombre = cliente.nombre,
-                email = cliente.gmail,
+            db.insertSocio(
+                nombre = socio.nombre,
+                gmail = socio.gmail,
                 created_at = timeStamp,
                 updated_at = timeStamp,
             )
         }
 
-        return db.selectClienteLastInserted().executeAsOne().toCliente()
+        return db.selectSocioLastInserted().executeAsOne().toSocio()
     }
 
     override fun update(id: Long, socio: Socio): Socio? {
-        logger.debug { "Actualizando cliente por id: $id" }
+        logger.debug { "Actualizando socio por id: $id" }
         var result = this.findById(id) ?: return null
         val timeStamp = LocalDateTime.now()
         result = result.copy(
@@ -50,7 +49,7 @@ class ClientesRepositoryImpl(
             updatedAt = timeStamp
         )
 
-        db.updateCliente(
+        db.updateSocio(
             nombre = result.nombre,
             gmail = result.gmail,
             updated_at = timeStamp.toString(),
@@ -61,7 +60,7 @@ class ClientesRepositoryImpl(
     }
 
     override fun delete(id: Long): Socio? {
-        logger.debug { "Borrando cliente por id: $id" }
+        logger.debug { "Borrando socio por id: $id" }
         val result = this.findById(id) ?: return null
         // Esto es borrado lógico
         val timeStamp = LocalDateTime.now()
