49b2a4def8f4e0a6ca9f1b529d4392e625df6d24
Samuel Cortés Sánchez <132077654+Samuceese@users.noreply.github.com>
2024-04-28 18:47:03 +0200

    Commit

--- Cine/src/main/kotlin/ventas/mappers/VentasMappers.kt
+++ Cine/src/main/kotlin/ventas/mappers/VentasMappers.kt
@@ -3,46 +3,46 @@ package ventas.mappers
 import Database.VentaEntity
 import productos.models.Complemento
 import socio.models.Socio
-import ventas.dto.VentasDto
+import ventas.dto.VentaDto
 import ventas.models.LineaVenta
 import ventas.models.Venta
 import java.time.LocalDateTime
 import java.util.*
 
-fun VentaEntity.Venta(complemento: Complemento): LineaVenta {
+fun VentaEntity.toVentaDto(complemento: Complemento): LineaVenta {
     return LineaVenta(
         id = UUID.fromString(this.id.toString()),
         lineas = lineas,
-        createAt = LocalDateTime.parse(this.createAt),
-        updateAt = LocalDateTime.parse(this.updateAt),
+        createdAt = LocalDateTime.parse(this.createdAt),
+        updatedAt = LocalDateTime.parse(this.updatedAt),
     )
 }
 
 fun VentaEntity.toVenta(socio: Socio, lineas: List<LineaVenta>): Venta {
     return Venta(
         id = UUID.fromString(this.id),
-        Socio = socio,
+        socio = socio,
         lineas = lineas,
-        createdAt = LocalDateTime.parse(this.createAt),
-        updatedAt = LocalDateTime.parse(this.updateAt),
+        createdAt = LocalDateTime.parse(this.createdAt),
+        updatedAt = LocalDateTime.parse(this.updatedAt),
     )
 }
 
-fun LineaVenta.toLineaVentaDto(): VentasDto {
-    return VentasDto(
+fun LineaVenta.toLineaVentaDto(): VentaDto {
+    return VentaDto(
         id = this.id.toString(),
         producto = this.producto.toProductoDto(),
         cantidad = this.cantidad,
         precio = this.precio,
-        createAt = this.createAt.toString(),
-        updateAt = this.updateAt.toString(),
+        createdAt = this.createdAt.toString(),
+        updatedAt = this.updatedAt.toString(),
     )
 }
 
-fun Venta.toVentaDto(): VentasDto {
-    return VentasDto(
+fun Venta.toVentaDto(): VentaDto {
+    return VentaDto(
         id = this.id.toString(),
-         = this.cliente.toClienteDto(),
+         = this.socio.toClienteDto(),
         lineas = this.lineas.map { it.toLineaVentaDto() },
         total = this.lineas.sumOf { it.precio },
         createdAt = this.createdAt.toString(),
